b4e9b7b1141dc6cdc62d009974ee0d7dd721b2cc
Vladimir <work.lvg@gmail.com>
2022-03-20 17:27:15 +0300

    size serializer

---
+++ src/Orc.Automation/Converters/SizeSerializationConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace Orc.Automation.Converters
+{
+    using System.Windows;
+
+    public class SerializableSize
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    internal class SizeSerializationConverter : SerializationValueConverterBase<Size, SerializableSize>
+    {
+        public override object ConvertFrom(Size value)
+        {
+            return new SerializableSize
+            {
+                Width = value.Width,
+                Height = value.Height
+            };
+        }
+
+        public override object ConvertTo(SerializableSize value)
+        {
+            return new Size
+            {
+                Width = value.Width,
+                Height = value.Height
+            };
+        }
+    }
+}
