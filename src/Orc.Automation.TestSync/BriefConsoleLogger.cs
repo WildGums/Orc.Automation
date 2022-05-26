0fbf11154d380ee055ae7f7097557e17e3ab46b4
Vladimir <work.lvg@gmail.com>
2022-05-27 02:33:02 +0300

    send test results unitlity

---
+++ src/Orc.Automation.TestSync/BriefConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿namespace Orc.Automation.TestSync
+{
+    using System;
+    using Catel.Logging;
+
+    public class BriefConsoleLogger : LogListenerBase
+    {
+        #region Methods
+        protected override void Write(ILog log, string message, LogEvent logEvent, object extraData, LogData logData, DateTime time)
+        {
+            if (string.Equals(log.Tag, "Orc.CommandLine.CommandLineParser"))
+            {
+                return;
+            }
+
+            switch (logEvent)
+            {
+                case LogEvent.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case LogEvent.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            Console.WriteLine("{0}", message);
+        }
+        #endregion
+    }
+}
---
+++ src/Orc.Automation.TestSync/Options.cs
@@ -0,0 +1,15 @@
+﻿namespace Orc.Automation.TestSync
+{
+    using CommandLine;
+
+    public class Options : ContextBase
+    {
+        #region Properties
+        [Option("p", "project", IsMandatory = true, HelpText = "jira project name")]
+        public string Project { get; set; }
+
+        [Option("t", "test", IsMandatory = true, HelpText = "test result file")]
+        public string OutputFolder { get; set; }
+        #endregion
+    }
+}
