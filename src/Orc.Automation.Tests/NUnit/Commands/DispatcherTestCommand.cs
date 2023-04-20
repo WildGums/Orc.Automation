d3941dbf6d3959e2f1b8ee0c64fcf94d8788d870
Vladimir <work.lvg@gmail.com>
2023-04-20 16:41:47 +0300

    add useful NUnit attributes

---
+++ src/Orc.Automation.Tests/NUnit/Commands/DispatcherTestCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace Orc.Automation.Tests;
+
+using Catel.Services;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+
+public class DispatcherTestCommand : DelegatingTestCommand
+{
+    public DispatcherTestCommand(TestCommand innerCommand)
+        : base(innerCommand)
+    {
+    }
+
+    public override TestResult Execute(TestExecutionContext context)
+    {
+        var dispatcherService = new DispatcherService(new DispatcherProviderService());
+
+        dispatcherService.Invoke(() =>
+        {
+            context.CurrentResult = innerCommand.Execute(context);
+        });
+
+        return context.CurrentResult;
+    }
+}
