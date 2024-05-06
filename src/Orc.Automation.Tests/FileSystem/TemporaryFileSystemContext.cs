0fc1f9b32bef7236018d17daf464eeeb86e2829f
Vladimir <work.lvg@gmail.com>
2024-05-06 19:58:33 +0300

    base tests class for applications
    Task - WildGums - FlexGrid - Fix bugs in Orc.Controls and Libraries concerning FG - 2024-05-06

---
+++ src/Orc.Automation.Tests/FileSystem/TemporaryFileSystemContext.cs
@@ -0,0 +1,229 @@
+﻿namespace Orc.Automation.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Catel.Logging;
+
+public sealed class TemporaryFileSystemContext : IDisposable
+{
+    private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+    private readonly string _moveDirectory;
+    private readonly List<BackupEntry> _registeredEntries = [];
+
+    public TemporaryFileSystemContext(string moveDirectory = null)
+    {
+        _moveDirectory = moveDirectory;
+
+        if (_moveDirectory is not null && !Directory.Exists(moveDirectory))
+        {
+            Directory.CreateDirectory(moveDirectory);
+        }
+    }
+
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    public void Dispose()
+    {
+        try
+        {
+            foreach (var registeredEntry in _registeredEntries.AsEnumerable().Reverse())
+            {
+                var oldPath = registeredEntry.OldPath;
+                if (File.Exists(oldPath))
+                {
+                    File.Delete(oldPath);
+                }
+
+                if (Directory.Exists(oldPath))
+                {
+                    Directory.Delete(oldPath, true);
+                }
+
+                var newPath = registeredEntry.NewPath;
+                if (File.Exists(newPath))
+                {
+                    File.Move(newPath, oldPath);
+                }
+                else if (Directory.Exists(newPath))
+                {
+                    CopyFilesRecursively(newPath, oldPath);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Failed to delete temporary files");
+        }
+    }
+
+    public string CreateFile(string filePath, string contents, FileSystemContextEntryAction action = FileSystemContextEntryAction.None)
+    {
+        var entry = new BackupEntry
+        {
+            OldPath = filePath
+        };
+
+        BackupFile(filePath, action);
+
+        File.WriteAllText(filePath, contents);
+
+        _registeredEntries.Add(entry);
+
+        return filePath;
+    }
+
+    public string CreateDirectory(string directory, FileSystemContextEntryAction action = FileSystemContextEntryAction.None)
+    {
+        var entry = new BackupEntry
+        {
+            OldPath = directory
+        };
+
+        BackupDirectory(directory, action);
+
+        Directory.CreateDirectory(directory);
+
+        _registeredEntries.Add(entry);
+
+        return directory;
+    }
+
+    public string Copy(string source, string destination, FileSystemContextEntryAction action = FileSystemContextEntryAction.None)
+    {
+        if (File.Exists(source))
+        {
+            BackupFile(destination, action);
+
+            File.Copy(source, destination);
+
+            return destination;
+        }
+
+        if (Directory.Exists(source))
+        {
+            BackupDirectory(destination, action);
+
+            CopyFilesRecursively(source, destination);
+
+            return destination;
+        }
+
+        throw Log.ErrorAndCreateException<Exception>($"Can't copy because path: '{source}' doesn't exist in file system");
+    }
+
+    private string GenerateUniqueMovePath()
+    {
+        if (_moveDirectory is null)
+        {
+            throw Log.ErrorAndCreateException<Exception>("Move directory wasn't specified");
+        }
+
+        return Path.Combine(_moveDirectory, Guid.NewGuid().ToString());
+    }
+
+    private void BackupFile(string filePath, FileSystemContextEntryAction action)
+    {
+        if (!File.Exists(filePath))
+        {
+            _registeredEntries.Add(new()
+            {
+                OldPath = filePath
+            });
+
+            return;
+        }
+
+        switch (action)
+        {
+            case FileSystemContextEntryAction.None:
+                throw Log.ErrorAndCreateException<Exception>($"File: '{filePath}' already exists");
+
+            case FileSystemContextEntryAction.Delete:
+                File.Delete(filePath);
+                _registeredEntries.Add(new()
+                {
+                    OldPath = filePath
+                });
+                break;
+
+            case FileSystemContextEntryAction.Move when _moveDirectory is null:
+                throw Log.ErrorAndCreateException<Exception>("Move directory wasn't specified");
+
+            case FileSystemContextEntryAction.Move:
+                var newPath = GenerateUniqueMovePath();
+                File.Move(filePath, newPath);
+
+                _registeredEntries.Add(new()
+                {
+                    OldPath = filePath,
+                    NewPath = newPath
+                });
+
+                break;
+        }
+    }
+
+
+    public void BackupDirectory(string directory, FileSystemContextEntryAction action)
+    {
+        if (!Directory.Exists(directory))
+        {
+            _registeredEntries.Add(new()
+            {
+                OldPath = directory
+            });
+
+            return;
+        }
+
+        switch (action)
+        {
+            case FileSystemContextEntryAction.None:
+                throw Log.ErrorAndCreateException<Exception>($"Directory: '{directory}' already exists");
+
+            case FileSystemContextEntryAction.Delete:
+                Directory.Delete(directory, true);
+                break;
+
+            case FileSystemContextEntryAction.Move when _moveDirectory is null:
+                throw Log.ErrorAndCreateException<Exception>("Move directory wasn't specified");
+
+            case FileSystemContextEntryAction.Move:
+                var newPath = GenerateUniqueMovePath();
+
+                Directory.Move(directory, newPath);
+
+                _registeredEntries.Add(new()
+                {
+                    OldPath = directory,
+                    NewPath = newPath
+                });
+
+                break;
+        }
+    }
+
+    private static void CopyFilesRecursively(string sourcePath, string targetPath)
+    {
+        foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+        }
+
+        //Copy all the files & Replaces any files with the same name
+        foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+        {
+            File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+        }
+    }
+
+    private class BackupEntry
+    {
+        public string OldPath { get; set; }
+        public string NewPath { get; set; }
+    }
+}
---
+++ src/Orc.Automation.Tests/TestFacts/Base/AppUiTestsBase.cs
@@ -0,0 +1,99 @@
+﻿namespace Orc.Automation.Tests;
+
+using System.Threading.Channels;
+using System.Windows.Automation;
+using Controls;
+using NUnit.Framework;
+
+public abstract class AppUiTestsBase<TMainWindow> : UiTestFactsBase
+    where TMainWindow : class
+{
+    #region Properties
+    protected abstract string AppName { get; }
+
+    protected virtual string NetVersion => "net8.0-windows";
+    protected virtual string Company => "Simply Effective Solutions";
+    protected virtual string Channel => "alpha";
+    protected abstract string RelativeProjectPath { get; }
+    protected abstract string RelativeAppConfigurationFolderPath { get; }
+
+    protected virtual string ShellWindowId => "ShellWindowId";
+    protected virtual string TestHostId => "TestHostId";
+    protected string AbsoluteProjectPath { get; private set; }
+    protected string AbsoluteAppConfigurationFolderPathProjectPath { get; private set; }
+
+    protected override string Args => "-p";
+    protected override string ExecutablePath => TestPathHelper.GetExecutablePath(AppName, NetVersion);
+    protected override Condition FindMainWindowCondition => new PropertyCondition(AutomationElement.AutomationIdProperty, ShellWindowId);
+
+    protected TemporaryFileSystemContext TempTestFileSystemContext { get; private set; }
+
+    protected TMainWindow MainWindow
+    {
+        get
+        {
+            if (Setup is null)
+            {
+                throw new("Can't setup test");
+            }
+
+            if (Setup.MainWindow is null)
+            {
+                throw new("Can't find main window");
+            }
+
+            return Setup.MainWindow.As<TMainWindow>();
+        }
+    }
+    #endregion
+
+    #region Methods
+    [OneTimeSetUp]
+    public override void SetUp()
+    {
+        var moveDirectory = TestPathHelper.GetTempDirectoryAbsolutePath(AppName, "MoveFolder");
+
+        TempTestFileSystemContext = new(moveDirectory);
+
+        TempTestFileSystemContext.CreateDirectory(moveDirectory, FileSystemContextEntryAction.Delete);
+
+        AbsoluteProjectPath = TempTestFileSystemContext.Copy(TestPathHelper.GetTestDirectoryAbsolutePath(RelativeProjectPath),
+            TestPathHelper.GetTempDirectoryAbsolutePath(AppName, GetType().Name + "_Project"), FileSystemContextEntryAction.Delete);
+
+        AbsoluteAppConfigurationFolderPathProjectPath = TempTestFileSystemContext.Copy(TestPathHelper.GetTestDirectoryAbsolutePath(RelativeAppConfigurationFolderPath),
+            TestPathHelper.GetConfigurationFolderPath(AppName), FileSystemContextEntryAction.Move);
+
+        base.SetUp();
+
+        LoadAssembly(TestPathHelper.GetAutomationTestAssemblyPath());
+        LoadAssembly(TestPathHelper.GetAppTestAssemblyPath(AppName));
+    }
+
+    [OneTimeTearDown]
+    public void TearDownTestSession()
+    {
+        try
+        {
+            Setup?.Dispose();
+
+            Wait.UntilInputProcessed(1000);
+
+            TempTestFileSystemContext.Dispose();
+        }
+        catch
+        {
+            // ignored
+        }
+        finally
+        {
+            Wait.UntilInputProcessed(1000);
+        }
+    }
+
+    protected void LoadAssembly(string assemblyPath)
+    {
+        var testHost = Setup?.MainWindow?.Find<TestHostAutomationControl>(TestHostId, numberOfWaits: 10);
+        testHost?.TryLoadAssembly(assemblyPath);
+    }
+    #endregion
+}
