38587dd2aa76670d83f4955778b33424cf967e88
Vladimir <work.lvg@gmail.com>
2024-09-30 19:39:35 +0300

    clean up

--- src/Orc.Automation/AutomationControls/Peers/Base/AutomationControlPeerBase.cs
+++ src/Orc.Automation/AutomationControls/Peers/Base/AutomationControlPeerBase.cs
@@ -308,8 +308,7 @@ public abstract class AutomationControlPeerBase : FrameworkElementAutomationPeer
 
     private bool HasAccess()
     {
-
-#pragma warning disable IDISP004 // Don't ignore created IDisposable
+        #pragma warning disable IDISP004 // Don't ignore created IDisposable
         var automationTestAccessService = this.GetServiceLocator().ResolveType<IAutomationTestAccessService>();
 #pragma warning restore IDISP004 // Don't ignore created IDisposable
 
